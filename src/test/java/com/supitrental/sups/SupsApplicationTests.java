c9f1c155e985be1e4c179501b9631b54a4861c9f
hariadivicky <hariadivicky@gmail.com>
2023-08-12 16:19:18 +0700

    initial commit

---
+++ src/test/java/com/supitrental/sups/SupsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supitrental.sups;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
